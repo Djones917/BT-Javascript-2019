934fc73b29034171e8ddc61f3b8dce4817dec8b6
Derek <nightwing917@hotmail.com>
2019-02-05 18:48:57 -0700

    starting BT javascript

---
+++ section_5/constructors@this/app.js
@@ -0,0 +1,30 @@
+// Object Literall - this is fine for one instance basic
+const personOne = {
+   name: 'Vincent',
+   age: 6
+}
+
+console.log(personOne);
+console.log(personOne.age);
+
+
+
+// Person constructor - CONSTRUCTORS START WITH A CAPITOL LETTER!!!
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
+  this.calculateAge = function(){
+    const diff =  Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+}
+
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 30);
+
+// console.log(john.age);
+
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
