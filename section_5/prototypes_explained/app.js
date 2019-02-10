859f108c38f644b6040a81c833e5a96fa1cf7f06
Derek <nightwing917@hotmail.com>
2019-02-09 20:02:15 -0700

    added person constructor

--- section_5/prototypes_explained/app.js
+++ section_5/prototypes_explained/app.js
@@ -1,1 +1,16 @@
-console.log('Connected!');
+//console.log('Connected!');
+
+// Object.prototype
+// Person.prototype
+
+// Person Constructor
+function Person(firstname, lastname, dob) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthday = new Date(dob);
+    this.calculateAge = function() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+}
