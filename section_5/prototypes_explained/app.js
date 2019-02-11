f2a366ab5b2ce70be65871b380c8b28dac5946b0
Derek <nightwing917@hotmail.com>
2019-02-10 19:47:37 -0700

    add get age prototype

--- section_5/prototypes_explained/app.js
+++ section_5/prototypes_explained/app.js
@@ -8,9 +8,26 @@ function Person(firstname, lastname, dob) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.birthday = new Date(dob);
-    this.calculateAge = function() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+    //this.calculateAge = function() {
+       // const diff = Date.now() - this.birthday.getTime();
+       // const ageDate = new Date(diff);
+       // return Math.abs(ageDate.getUTCFullYear() - 1970);
+   // }
 }
+
+
+// Calulate age - move this out of the constructor because it does the same thing over and over.
+Person.prototype.calculateAge = function () {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+
+
+// __prototype__ object and person
+// instantiate instance
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+console.log(mary);
