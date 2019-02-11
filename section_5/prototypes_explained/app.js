2f78358f0291da02370066f4fc8c495f0291cc55
Derek <nightwing917@hotmail.com>
2019-02-10 19:57:33 -0700

    added get full name

--- section_5/prototypes_explained/app.js
+++ section_5/prototypes_explained/app.js
@@ -23,7 +23,10 @@ Person.prototype.calculateAge = function () {
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-
+// Lets build another prototype with name
+Person.prototype.getFullName = function() {
+    return `${this.firstname} ${this.lastname}`;
+}
 
 // __prototype__ object and person
 // instantiate instance
@@ -31,3 +34,9 @@ const john = new Person('John', 'Doe', '8-12-90');
 const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
 console.log(mary);
+
+// Lets call calculate age now that it is out of the constructor
+console.log(john.calculateAge()); 
+
+// Lets call get full name
+console.log(mary.getFullName());
