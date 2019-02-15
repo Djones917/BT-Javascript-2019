081595756f147b75ae804377a285774397bf7852
Derek <nightwing917@hotmail.com>
2019-02-14 19:15:22 -0700

    added prototype method full name

--- section_5/prototypal_inheritance/app.js
+++ section_5/prototypal_inheritance/app.js
@@ -5,3 +5,13 @@ function Person(firstName, lastName) {
    this.firstName = firstName;
    this.lastName = lastName;
 }
+
+// Greeting "prototype method"
+Person.prototype.greeting = function() {
+    return `Hello there, ${this.firstName} ${this.lastName}`;
+}
+
+
+const person1 = new Person('John', 'Doe');
+
+console.log(person1.greeting());
