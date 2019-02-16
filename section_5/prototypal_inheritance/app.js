5fc521a69c9666183f196f4fbb4c6e5ba771dc8e
Derek <nightwing917@hotmail.com>
2019-02-15 19:40:42 -0700

    prototype inheritence finished

--- section_5/prototypal_inheritance/app.js
+++ section_5/prototypal_inheritance/app.js
@@ -30,9 +30,17 @@ function Customer(firstName, lastName, phone, membership) {
 // Inherit the Person prototype methods
 Customer.prototype = Object.create(Person.prototype);
 
+// Make customer.prototype return Customer()
+Customer.prototype.constuctor = Customer;
+
 //  Create Customer
 const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard' );
 
 console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function() {
+   return `Hello there, ${this.firstName} ${this.lastName} welcome to our company!`;
+}
 
 console.log(customer1.greeting());
