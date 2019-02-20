762f0122727f76f882a62d98f58bd8b66d70cf0a
Derek <nightwing917@hotmail.com>
2019-02-19 19:41:14 -0700

    added mary

--- section_5/ES6_Classes/app.js
+++ section_5/ES6_Classes/app.js
@@ -1,1 +1,11 @@
-console.log('Connected!');
+//console.log('Connected!');
+
+// create class ES6 
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+const mary = new Person('Mary', 'Williams');
