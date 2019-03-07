fec38e0a3253ad5767b8aa55a27daad5fd248491
Derek <nightwing917@hotmail.com>
2019-03-06 19:23:51 -0700

    init var

--- section-2/variables/app.js
+++ section-2/variables/app.js
@@ -1,7 +1,14 @@
 // var, let, const
 
+// Remember you can do this with var and let
 var name = 'Boo Who';
 console.log(name); // will print Boo who
 name = 'Jane Smith';
 console.log(name); // will print Jane Smith
 
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
