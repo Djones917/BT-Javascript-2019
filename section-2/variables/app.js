917632a4e09aa240dd494ab0f45c41b97502bcf8
Derek <nightwing917@hotmail.com>
2019-03-07 19:22:57 -0700

    adding multi var names

--- section-2/variables/app.js
+++ section-2/variables/app.js
@@ -7,8 +7,17 @@ name = 'Jane Smith';
 console.log(name); // will print Jane Smith
 
 
-// Init var
+// Init var - example an if statement!
 var greeting;
 console.log(greeting);
 greeting = 'Hello';
 console.log(greeting);
+
+// Letters, Numbers, _, $ - you might use $ with jquery other than that pass on it
+// Can not start with a number
+
+
+// Multi word variables - camel case
+var firstName = 'John'; 
+var first_name = 'Sarah'; // Underscore - you will see this most likely in php
+var FirstName = 'Bob'; // Pascal Case - it's the same as camel case except the first letter is capitalized
