e9838daa92624dc4a097febcf4c8102a143417c6
Derek <nightwing917@hotmail.com>
2019-03-08 19:44:42 -0700

    adding data types

---
+++ section-2/data types/app.js
@@ -0,0 +1,11 @@
+//console.log('connected!');
+
+// Primitive
+
+// String
+const name = 'John Doe';
+// Number
+const age = 7;
+// Boolean
+const lovesMovies = true;
+console.log(typeof lovesMovies);
