d76705453ea0ca649ec274edaf2027cc00fcefee
Derek <nightwing917@hotmail.com>
2019-03-12 20:20:40 -0600

    converted number to a string

--- section-2/type conversion/app.js
+++ section-2/type conversion/app.js
@@ -1,1 +1,15 @@
-console.log('Connected');
+//console.log('Connected');
+
+let val;
+
+// Number to string
+// val = 5;  This will not work with length
+val = String(5); // This will turn your number to a string and length will work! Black print is a string in the console, Blue is a number!
+val = String(4 + 4); // This will override the above val and print 1 on .length because 4+4=8
+
+// Output
+console.log(val);
+console.log(typeof val);
+console.log(val.length); // This will kick back undefined length only works on a string
+
+
