b3d38ee5c76226de8e8478ef01ecea1bf4309540
Derek <nightwing917@hotmail.com>
2019-03-13 20:09:48 -0600

    finished converting to string

--- section-2/type conversion/app.js
+++ section-2/type conversion/app.js
@@ -7,6 +7,20 @@ let val;
 val = String(5); // This will turn your number to a string and length will work! Black print is a string in the console, Blue is a number!
 val = String(4 + 4); // This will override the above val and print 1 on .length because 4+4=8
 
+// Boolean to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
+
+
+
+// toString() method
+
+
+
+
 // Output
 console.log(val);
 console.log(typeof val);
