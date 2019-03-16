bb8be89074a4cb2136503b4b2ebbb20e363feb7e
Derek <nightwing917@hotmail.com>
2019-03-15 19:12:23 -0600

    added parseInt and parseFloat

--- section-2/type conversion/app.js
+++ section-2/type conversion/app.js
@@ -41,7 +41,8 @@ val1 = Number('5');
 // val1 = Number('Hello'); This will return NaN - not a number!
 // val1 = Number([1,2,3,4]); This will return NaN - not a number!
 
-
+val1 = parseInt('100.30'); // This will give you the number 100 because it parses as an interger
+val1 = parseFloat('100.30'); // This will give you the number 100.3 one decimal as a float but only because it's a zero! if it was 100.31 that what it would print!
 
 console.log(val1);
 console.log(typeof val1);
