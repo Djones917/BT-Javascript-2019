073948f4417ad04756223712b1e920c9adab6753
Derek <nightwing917@hotmail.com>
2019-03-17 20:17:13 -0600

    added lastIndexOf

--- section-2/String Methods_concatenation/app.js
+++ section-2/String Methods_concatenation/app.js
@@ -1,1 +1,40 @@
-console.log('Connected!');
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 40;
+
+let val;
+
+val = firstName + lastName;
+
+// Concatenation
+val = firstName + ' ' + lastName;
+
+// Append
+val = 'Derek '; // space at the end
+val += 'Jones';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping - you could use double quotes but back splash is better
+val = 'That\'s awesome, I can\'t wait!;
+
+// Length
+val = firstName.length;
+
+// Concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+
+
+// Not very useful but good to know
+val = firstName[0];
+
+// indexOf()
+val =firstName.indexOf('2');
+val = firstName.lastIndexOf('2');
+
+console.log(val);
