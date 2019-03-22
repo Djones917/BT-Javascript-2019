f28435ff476668033e4bbb915b91983b1afb2434
Derek <nightwing917@hotmail.com>
2019-03-21 19:10:48 -0600

    without template strings

--- section-2/template_literals/app.js
+++ section-2/template_literals/app.js
@@ -1,1 +1,10 @@
-console.log('Connected!');
+const name = 'John';
+const age = 30;
+const job = 'Web Developer';
+const city = 'Salt Lake City';
+let html;
+
+// Without template strings (es5)
+html = '<ul><li>Name: ' + name +'</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
+
+document.body.innerHTML = html;
