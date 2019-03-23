c65cd9f0270d9a1b6fb101b62a0e817bbe88c411
Derek <nightwing917@hotmail.com>
2019-03-23 16:10:43 -0600

    adding arrays

--- section-2/arrays_and_methods/app.js
+++ section-2/arrays_and_methods/app.js
@@ -1,1 +1,6 @@
-console.log('Connected!');
+// Create some arrays
+const numbers = [43, 56, 33, 12, 9, 1, 22];
+// Second way to create array is the array constructor
+const numbers2 = new Array(6, 8, 2, 19, 21, 5, 4);
+// Array of strings
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
