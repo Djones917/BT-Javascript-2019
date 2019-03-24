ecc18726abdc56b1ce67c9742e4c2dbc26de428b
nightwing917@hotmail.com <nightwing917@hotmail.com>
2019-03-24 14:58:18 -0600

    inserting into array

--- section-2/arrays_and_methods/app.js
+++ section-2/arrays_and_methods/app.js
@@ -4,3 +4,24 @@ const numbers = [43, 56, 33, 12, 9, 1, 22];
 const numbers2 = new Array(6, 8, 2, 19, 21, 5, 4);
 // Array of strings
 const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+// Mixed array
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:2}, new Date()];
+
+
+
+let val;
+
+
+
+// Get array length
+val = numbers.length;
+// Check if it is an array
+val = Array.isArray(numbers);
+// Get a single value in array NOTE: Remember arrays are 0 based!
+val = numbers[3];
+// Insert into array
+numbers[2] = 100;
+
+
+console.log(numbers);
+console.log(val);
