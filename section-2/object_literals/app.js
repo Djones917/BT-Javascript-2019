19501420dc2b831186ec56037f40ae1c2f7eb4cf
Derek <nightwing917@hotmail.com>
2019-03-26 20:16:03 -0600

    adding object literals

--- section-2/object_literals/app.js
+++ section-2/object_literals/app.js
@@ -1,1 +1,28 @@
-console.log('Connected!');
+// Object literal
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 30,
+    email: 'steve@aol.com',
+    hobbies: ['Music', 'Sports'],
+    address: {
+        city: 'Miami',
+        state: 'FL'
+    },
+    getBirthYear: function() {
+        return 1987;
+    }
+}
+
+
+let val;
+
+val = person;
+
+// Get specific value
+// Dot notation - recommended!
+val = person.firstName;
+// Bracket notation
+val = person['firstName'];
+
+console.log(val);
