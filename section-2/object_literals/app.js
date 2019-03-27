0355bd3d698e2ffbee3d5deb4c28ad1a30c04925
Derek <nightwing917@hotmail.com>
2019-03-27 09:09:04 -0600

    still working on object

--- section-2/object_literals/app.js
+++ section-2/object_literals/app.js
@@ -24,5 +24,13 @@ val = person;
 val = person.firstName;
 // Bracket notation
 val = person['firstName'];
+val = person.lastName;
+//val = person.hobbies;
+val = person.hobbies[1];
+//val = person.address;
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear(); 
+
 
 console.log(val);
