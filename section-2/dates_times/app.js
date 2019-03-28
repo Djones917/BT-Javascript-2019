75424a03a126476b98631298e1277940533f07a3
Derek <nightwing917@hotmail.com>
2019-03-27 19:43:50 -0600

    getDate add or get day

--- section-2/dates_times/app.js
+++ section-2/dates_times/app.js
@@ -2,6 +2,13 @@ let val;
 
 const today = new Date();
 
-val = today;
+// This will print that date! dates can be written different ways and still work 9-10-81 and 9/10/81 and September 10 1981.
+let birthday = new Date('9-10-81 11:25:00'); // The 11:25:00 is the time and you can set that too!
 
+//val = today;
+val = today.getMonth(); // Months are zero based January = 0
+// Get the day of the month
+val = today.getDate();
+
+// If you add typeof you will get an object but this will give today's date!
 console.log(val);
