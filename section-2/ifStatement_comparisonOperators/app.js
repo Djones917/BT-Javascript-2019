9c55f8e32fe7b5eb92ad8e6fee079a6d836862d5
Derek <nightwing917@hotmail.com>
2019-03-29 19:32:23 -0600

    starting if statements

---
+++ section-2/ifStatement_comparisonOperators/app.js
@@ -0,0 +1,41 @@
+ //   if(something) {
+ //   do something
+ //   } else {
+ //       do something else
+ //   }
+
+
+ const id = 100;
+
+
+
+ // Equal to  NOTE: = is to assign and == is equal to and === is equal and type!
+ if(id == 100) {
+   console.log('Correct!');
+ } else {
+    console.log('Incorrect!'); 
+ }
+
+
+
+
+ // Not Equal NOTE: = is to assign and != is equal to and !== is equal and type!
+if (id != 100) {
+    console.log('Correct!');
+} else {
+    console.log('Incorrect!');
+}
+
+
+
+
+// Equal to Value and Type NOTE: MOST OF THE TIME THIS IS BETTER AS IT PREVENTS ERRORS LATER ON!
+if (id === 100) {
+    console.log('Correct!');
+} else {
+    console.log('Incorrect!');
+}
+
+
+
+
