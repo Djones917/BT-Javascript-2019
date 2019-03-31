699079da138e7badc8d8573a713e5e151e5a2316
Derek <nightwing917@hotmail.com>
2019-03-31 17:02:22 -0600

    commented out examples to move on

--- section-2/ifStatement_comparisonOperators/app.js
+++ section-2/ifStatement_comparisonOperators/app.js
@@ -10,41 +10,41 @@
 
 
  // Equal to  NOTE: = is to assign and == is equal to and === is equal and type!
- if(id == 100) {
-   console.log('Correct!');
- } else {
-    console.log('Incorrect!'); 
- }
+// if(id == 100) {
+  // console.log('Correct!');
+ //} else {
+  //  console.log('Incorrect!'); 
+ //}
 
 
 
 
  // Not Equal NOTE: = is to assign and != is equal to and !== is equal and type!
-if (id != 100) {
-    console.log('Correct!');
-} else {
-    console.log('Incorrect!');
-}
+//if (id != 100) {
+ //   console.log('Correct!');
+//} else {
+ //   console.log('Incorrect!');
+//}
 
 
 
 
 // Equal to Value and Type NOTE: MOST OF THE TIME THIS IS BETTER AS IT PREVENTS ERRORS LATER ON!
-if (id === 100) {
-    console.log('Correct!');
-} else {
-    console.log('Incorrect!');
-}
+//if (id === 100) {
+ //   console.log('Correct!');
+//} else {
+//    console.log('Incorrect!');
+//}
 
 
 
 
 // Not equal to Value and Type NOTE: MOST OF THE TIME THIS IS BETTER AS IT PREVENTS ERRORS LATER ON!
-if (id !== 100) {
-    console.log('Correct!');
-} else {
-    console.log('Incorrect!');
-}
+//if (id !== 100) {
+//    console.log('Correct!');
+//} else {
+ //   console.log('Incorrect!');
+//}
 
 
 
