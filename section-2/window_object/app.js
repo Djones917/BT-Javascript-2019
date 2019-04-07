b1196071151806c16056ca0323f415273ee0d99e
Derek <nightwing917@hotmail.com>
2019-04-06 19:11:48 -0600

    added location objects

---
+++ section-2/window_object/app.js
@@ -0,0 +1,54 @@
+// Window Object - document object is part of the window object
+// In the console if you type window it will bring up all of the window properties
+// Windows Methods / Objects / Properties
+
+
+// Console.log - You could do this window.console.log('Hello');
+// But this is better
+//console.log(123);
+
+
+// Alert - window.alert();
+//alert('Hello, World');
+
+
+// Prompt
+//const input = prompt();
+//alert(input);
+
+
+// Confirm - often used when deleting something
+//if(confirm('Are you sure?')) {
+//  console.log('Yes');
+//} else {
+//   console.log('NO'); 
+//}
+
+
+
+
+
+let val;
+
+// Outer Height and Width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner Height and Width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points - Y is vertical X is horizontal - This is good for when you are building a site and you need to know when to start an animation ect.
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Objects
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+
+
+console.log(val);
