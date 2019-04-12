3c74eeb41f4481230a762fa91a24a41072437031
Derek <nightwing917@hotmail.com>
2019-04-11 19:37:14 -0600

    access the body

--- section-3/Dom_manipulation_Events/app.js
+++ section-3/Dom_manipulation_Events/app.js
@@ -2,5 +2,18 @@
 
 let val;
 
+// Gives us the entire html document view in console
+val = document;
+// Gives us a collection of everything in document
+val = document.all;
+// Get what you want zero based index
+val = document.all[0]; 
+// To see how many elements are in the DOM (43 with this html file)
+val = document.all.length;
+// Access the head
+val = document.head;
+// Access the body
+val = document.body;
+
 
 console.log(val);
