58fa743e0d0c2f099785d8db8525cb6af11a2bae
Derek <nightwing917@hotmail.com>
2019-04-19 20:46:41 -0600

    lesson on query selector to continue in next lesson

--- section-3/DomSelectorForSingleElements/app.js
+++ section-3/DomSelectorForSingleElements/app.js
@@ -61,3 +61,19 @@
 // document.querySelector() - Newer and more powerful because you can select both ID and Classes!
 
 console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+// Remember if there is more than one h5 it's only going to get the first one!
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+// Use CSS last child
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child()').textContent = 'Hello World';
+// Careful with this one it's a CSS nth-child issue!!!   NOTE: WILL BE CONTINUED IN THE NEXT LESSON!
+document.querySelector('li:nth-child(odd)').style.color = '#ccc';
+document.querySelector('li:nth-child(even)').style.color = '#f4f4f4';
+
+
+
