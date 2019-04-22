adc3b987865a353c40e446c5ac15597b5071359c
Derek <nightwing917@hotmail.com>
2019-04-21 20:07:04 -0600

    getting one item with get by class name

--- section-3/DomSelectorForMultipleElements/app.js
+++ section-3/DomSelectorForMultipleElements/app.js
@@ -1,1 +1,6 @@
-console.log('Connected!');
+// document.getElementsByClassName(); This will give us an HTML Collection or index of all class name items
+
+const items = document.getElementsByClassName('collection-item');
+console.log(items);
+console.log(items[0]);
+items[0].style.color = 'red';
