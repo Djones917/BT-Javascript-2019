94a49597cd575eae159ae41a10e645731d8ee41e
Derek <nightwing917@hotmail.com>
2019-04-21 20:30:40 -0600

    converting HTML collection to array with get element by tag name

--- section-3/DomSelectorForMultipleElements/app.js
+++ section-3/DomSelectorForMultipleElements/app.js
@@ -1,6 +1,34 @@
-// document.getElementsByClassName(); This will give us an HTML Collection or index of all class name items
+// document.getElementsByClassName(); This will give us an HTML Collection  or index of all class name items (NOTE: HTML COLLECTION IS NOT AN ARRAY!!!!)
 
-const items = document.getElementsByClassName('collection-item');
-console.log(items);
-console.log(items[0]);
-items[0].style.color = 'red';
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello World';
+
+// Another way to select
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
+
+
+
+
+
+
+
+
+// getElementsByTagName()
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello World';
+
+
+// TURN HTML COLLECTION INTO AN ARRAY! (NOTE: HTML COLLECTION IS NOT AN ARRAY!!!!) Remeber use let to change something as const will not let you!
+lis = Array.from(lis);
+lis.reverse();
+
+console.log(lis);
