6f73f44e294bc9f9ac82972cb05a9eb17f297e98
Derek <nightwing917@hotmail.com>
2019-04-24 19:09:36 -0600

    added query selector

--- section-3/TraversingTheDom/app.js
+++ section-3/TraversingTheDom/app.js
@@ -1,1 +1,11 @@
 // Traversin the DOM - Move up and down throught the DOM Working with parents and children
+
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem
+val = list
+
+console.log(val);
