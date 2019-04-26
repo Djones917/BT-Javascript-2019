0d86edd97a132be0ba5ead3bfcd208692202537e
Derek <nightwing917@hotmail.com>
2019-04-25 19:36:31 -0600

    adding child node

--- section-3/TraversingTheDom/app.js
+++ section-3/TraversingTheDom/app.js
@@ -5,7 +5,15 @@ let val;
 const list = document.querySelector('ul.collection');
 const listItem = document.querySelector('li.collection-item:first-child');
 
-val = listItem
-val = list
+val = listItem;
+val = list;
+
+// Get child nodes NOTE: Returns a node list
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+
+// Get child element nodes NOTE: Returns a HTML collection
+val = list;
 
 console.log(val);
