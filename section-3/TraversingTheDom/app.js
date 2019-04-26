8ffc981e1bda830f9c363bce8d319cfbd37164c2
Derek <nightwing917@hotmail.com>
2019-04-25 19:58:59 -0600

    adding children of children

--- section-3/TraversingTheDom/app.js
+++ section-3/TraversingTheDom/app.js
@@ -12,8 +12,22 @@ val = list;
 val = list.childNodes;
 val = list.childNodes[0];
 val = list.childNodes[0].nodeName;
+val = list.childNodes[1].nodeType; // This will return a number Refer to the comments below
 
-// Get child element nodes NOTE: Returns a HTML collection
-val = list;
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+
+
+// Get child element nodes NOTE: Returns a HTML collection - You wil use this more childNode above!
+val = list.children;
+val = list.children[0];
+val = list.children[1].textContent = 'Hello';
+// Children of children
 
 console.log(val);
