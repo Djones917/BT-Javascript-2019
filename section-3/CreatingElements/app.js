0111b2219de0d5e157aeb934b3993ba93a44dfc0
Derek <nightwing917@hotmail.com>
2019-04-29 19:33:05 -0600

    appending li as child to ul

--- section-3/CreatingElements/app.js
+++ section-3/CreatingElements/app.js
@@ -13,5 +13,8 @@ li.setAttribute('title', 'New Item');
 // Create text node and append
 li.appendChild(document.createTextNode('Hello World'));
 
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild('li');
+
 
 console.log(li);
