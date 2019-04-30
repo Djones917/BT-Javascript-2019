8ab0182c38d9fa30bc8a3bf372db20f323e0373c
Derek <nightwing917@hotmail.com>
2019-04-29 20:06:21 -0600

    creating elements finished

--- section-3/CreatingElements/app.js
+++ section-3/CreatingElements/app.js
@@ -13,6 +13,20 @@ li.setAttribute('title', 'New Item');
 // Create text node and append
 li.appendChild(document.createTextNode('Hello World'));
 
+
+
+// Create new link element
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML('<i class="fa fa-remove"></i>');
+// Append link into li
+li.appendChild(link);
+
+
+
+
 // Append li as child to ul
 document.querySelector('ul.collection').appendChild('li');
 
