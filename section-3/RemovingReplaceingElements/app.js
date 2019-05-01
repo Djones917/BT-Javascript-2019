d8926f3ccf5fef962433b0f6a8f55f98f1a6e579
Derek <nightwing917@hotmail.com>
2019-04-30 20:17:46 -0600

    getting old heading

--- section-3/RemovingReplaceingElements/app.js
+++ section-3/RemovingReplaceingElements/app.js
@@ -3,4 +3,13 @@
 // Create Element
 const newHeading = document.createElement('h2');
 
+// Add ID
+newHeading.id = 'task-title';
+
+// New Text Node
+newHeading.appendChild(document.createTextNode('Task List'));
+
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+
 console.loog(newHeading);
