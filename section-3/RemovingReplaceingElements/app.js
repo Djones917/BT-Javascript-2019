81cffefc44ed11ac6a886093c06ed32e96ce13b5
Derek <nightwing917@hotmail.com>
2019-05-01 19:19:07 -0600

    added replaceChild

--- section-3/RemovingReplaceingElements/app.js
+++ section-3/RemovingReplaceingElements/app.js
@@ -11,5 +11,10 @@ newHeading.appendChild(document.createTextNode('Task List'));
 
 // Get the old heading
 const oldHeading = document.getElementById('task-title');
+// Parent
+const cardAction = document.querySelector('.card-action');
+
+// Replace
+cardAction.replaceChild(newHeading, oldHeading);
 
 console.loog(newHeading);
