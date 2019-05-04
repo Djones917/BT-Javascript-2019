24544a6b80581ef4935ef9054eb9c33bc5c585c6
Derek <nightwing917@hotmail.com>
2019-05-03 20:30:32 -0600

    added the e object and e.preventDeafault

--- section-3/EventListeners_TheEventObject/app.js
+++ section-3/EventListeners_TheEventObject/app.js
@@ -1,1 +1,9 @@
-console.log('Connected!');
+// NOTE: WHEN YOU CLICK THE 'CLICKED' WILL FLASH REAL QUICK IN THE CONSOLE. TO STOP THIS PUT # IN THE HREF LINK. THIS IS BECAUSE SOME HTML ELEMENTS HAVE CERTAIN DEFAULT BEHAVIORS 
+// EXAMPLE A FORM WILL SUBMIT TO A PAGE, A LINK WILL TRY TO REDIRECT SO USE # 
+// TO STOP ANY DEFAULT BEHAVIOR ADD e (event object) to the function parameter and e.preventDefault()
+
+document.querySelector('.clear-tasks').addEventListener('click', function(e){
+    console.log('Clicked!');
+
+    e.preventDefault();
+});
