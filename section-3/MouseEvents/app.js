2838134c27dbd45ee2d434329d0a27ee9ee615e3
Derek <nightwing917@hotmail.com>
2019-05-07 19:49:00 -0600

    finished mouse up

--- section-3/MouseEvents/app.js
+++ section-3/MouseEvents/app.js
@@ -4,11 +4,16 @@ const heading = document.querySelector('h5');
 
 
 // Click
-clearBtn.document.addEventListener('click', runEvent);
+//clearBtn.document.addEventListener('click', runEvent);
+
 // Double Click
-clearBtn.document.addEventListener('dblclick', runEvent);
+//clearBtn.document.addEventListener('dblclick', runEvent);
 
+// Mouse Down -NOTE: TO FIRE MOUSE DOWN IT"S A CLICK AND HOLD!!!!
+clearBtn.document.addEventListener('mousedown', runEvent);
 
+// Mouse up -NOTE: TO FIRE MOUSE up IT"S A CLICK AND HOLD THEN LET GO AND IT WILL FIRE!!!!
+clearBtn.document.addEventListener('mousedown', runEvent);
 
 // Event Handler
 function runEvent(e) {
