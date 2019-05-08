4dc0ec82480eecb29f61689a3f827d9ab1556e5b
Derek <nightwing917@hotmail.com>
2019-05-07 19:28:25 -0600

    added event listener

--- section-3/MouseEvents/app.js
+++ section-3/MouseEvents/app.js
@@ -1,3 +1,15 @@
 const clearBtn = document.querySelector('.clear-task');
 const card = document.querySelector('.card');
 const heading = document.querySelector('h5');
+
+
+// Click
+clearBtn.document.addEventListener('click', runEvent);
+
+
+
+// Event Handler
+function runEvent(e) {
+   console.log(`EVENT TYPE: ${e.type}`);
+}
+
