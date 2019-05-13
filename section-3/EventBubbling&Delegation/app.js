1cc45a2fafa4eadc610d85dd8e54cf4a935d3c74
Derek <nightwing917@hotmail.com>
2019-05-12 20:44:05 -0600

    finished example of event bubbling

--- section-3/EventBubbling&Delegation/app.js
+++ section-3/EventBubbling&Delegation/app.js
@@ -1,1 +1,28 @@
-console.log('Connected!');
+// Event bubbling up of events through the Dom to it's parent.
+// Event Bubbling Remember bubbles up from child all the way up to parent!
+//document.querySelector('.card-title').addEventListener('click', function(){
+//    console.log('Card Title');
+//});
+
+//document.querySelector('.card-content').addEventListener('click', function () {
+//    console.log('Card Content');
+//});
+
+//document.querySelector('.card').addEventListener('click', function () {
+//    console.log('Card');
+//});
+
+//document.querySelector('.col').addEventListener('click', function () {
+//    console.log('COL');
+//});
+
+
+
+
+
+
+
+
+
+
+// Event Delegation is the opposite of Event bubbling with delegation we put the event listener on the parent and use logic to target the element.
