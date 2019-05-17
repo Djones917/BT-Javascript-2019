e92b3e0f61b3a898fc4d9856cfb61a81a22e2d13
Derek <nightwing917@hotmail.com>
2019-05-16 19:14:00 -0600

    cleared local storage

--- section-3/Local&SessionStorage/app.js
+++ section-3/Local&SessionStorage/app.js
@@ -1,7 +1,26 @@
 // NOTE: THE DIFFERENCE BETWEEN LOCAL STORAGE AND SESSION STORAGE IS SESSION CLEARS WHEN YOU CLOSE BROWSER. LOCAL HAS TO BE CLEARED MANUALLY
 
 // Set local storage item 
-localStorage.setItem('name', 'John');
+//localStorage.setItem('name', 'John');
+
 
 // Set session storage item 
-sessionStorage.setItem('name', 'Beth');
+//sessionStorage.setItem('name', 'Beth');
+
+
+// Remove from storage  
+//localStorage.removeItem('name');
+
+
+// Get from storage
+//localStorage.setItem('name', 'John');
+//localStorage.setItem('age', '30');
+//const name = localStorage.getItem('name');
+//const age = localStorage.getItem('age');
+
+
+// Clear from local storage
+//localStorage.clear();
+//console.log(name, age);
+
+
