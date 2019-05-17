9b1378bb9f3b33914caa08a8ea88a43aa5870734
Derek <nightwing917@hotmail.com>
2019-05-16 18:59:36 -0600

    starting local storage

---
+++ section-3/Local&SessionStorage/app.js
@@ -0,0 +1,7 @@
+// NOTE: THE DIFFERENCE BETWEEN LOCAL STORAGE AND SESSION STORAGE IS SESSION CLEARS WHEN YOU CLOSE BROWSER. LOCAL HAS TO BE CLEARED MANUALLY
+
+// Set local storage item 
+localStorage.setItem('name', 'John');
+
+// Set session storage item 
+sessionStorage.setItem('name', 'Beth');
