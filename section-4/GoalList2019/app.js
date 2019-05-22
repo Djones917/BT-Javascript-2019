fd06e852cd6bfe0354d0fc375276c9a38fec15cd
Derek <nightwing917@hotmail.com>
2019-05-21 18:21:39 -0600

    defining UI Variables

--- section-4/GoalList2019/app.js
+++ section-4/GoalList2019/app.js
@@ -1,1 +1,8 @@
-console.log('Connected!');
+// Define UI variables
+const form = document.querySelector('#goal-form');
+const goalList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-goals');
+const filter = document.querySelector('#filter');
+const goalInput = document.querySelector('#goal');
+console.log(goalInput);
+
