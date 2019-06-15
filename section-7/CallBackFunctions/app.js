23a57f2fb5523b400c5c3925827f56e0a3906a19
Derek <nightwing917@hotmail.com>
2019-06-14 20:02:25 -0600

    starting to work on call backs

--- section-7/CallBackFunctions/app.js
+++ section-7/CallBackFunctions/app.js
@@ -4,17 +4,56 @@ const posts = [
   { title: 'Post Two', body: 'This is post Two' }
 ];
 
+// This was created synchronous it takes 2 seconds for server to make create post and 1 second for it to get post but post three will not show up!
+// Create Posts
+//function createPosts() {
+  
+  //setTimeout(function(post) {
+    //posts.push(post);
+  //}, 2000);
+//}
+
+
+// Get Posts
+//function getPosts() {
+  //setTimeout(function() {
+    //let output = '';
+    //posts.forEach(function(post) {
+      //output += `<li>${post.title}</li>`;
+    //});
+    //document.body.innerHTML = output;
+  //}, 1000);
+//}
+
 
 // Create Posts
+//createPosts({title: 'Post Three', body: 'This is post Three'});
+
+
+//getPosts();
+
+
+
+
+
+
+// Now let's do this asynchronous with callbacks!
+// Create Posts
 function createPosts() {
-  
-  setTimeout(function(post){
+
+  setTimeout(function (post) {
     posts.push(post);
-  });
+  }, 2000);
 }
 
 
 // Get Posts
 function getPosts() {
-
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
