0fb644a8a9e253fb101a3ae0cd0e53d5b9990c93
Derek <nightwing917@hotmail.com>
2019-06-14 19:40:24 -0600

    create posts

--- section-7/CallBackFunctions/app.js
+++ section-7/CallBackFunctions/app.js
@@ -1,1 +1,20 @@
-console.log('Connected!');
+// This is going to mimick our server/database
+const posts = [
+  {title: 'Post One', body: 'This is post One'},
+  { title: 'Post Two', body: 'This is post Two' }
+];
+
+
+// Create Posts
+function createPosts() {
+  
+  setTimeout(function(post){
+    posts.push(post);
+  });
+}
+
+
+// Get Posts
+function getPosts() {
+
+}
