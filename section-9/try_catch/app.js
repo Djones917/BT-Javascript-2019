b0536b9c22b82bd27c0af2b6f08e3ac348921e98
Derek <nightwing917@hotmail.com>
2019-07-17 18:51:06 -0600

    added type error

--- section-9/try_catch/app.js
+++ section-9/try_catch/app.js
@@ -1,10 +1,24 @@
 //console.log('Connected!');
 
 // This is a basic "try and catch" this helps trouble shoot code the "e" is not for "event" it is for "error" or "err" catch takes that argument of "e"
-
 try {
   // produce a reference error
   myFunction();
 } catch(e) {
-  console.log(e);
+  //console.log(e);
+
+  // This will work too it will just hide error in console and just show message
+  //console.log(e.message);
+
+  // This will give name of error 
+  //console.log(e.name);
+
+  // This will give referenceerror
+  console.log(e instanceof ReferenceError);
+  
+  // This will give Typeerror
+  console.log(e instanceof TypeError);
 }
+
+// The nice thing about try and catch is that you don't have to stop the whole script from running the console.log below works
+console.log('Program continues...');
