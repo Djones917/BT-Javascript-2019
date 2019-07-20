ca9da0716de9b6b33b32dd545959b79f651a623d
Derek <nightwing917@hotmail.com>
2019-07-19 19:25:46 -0600

    starting part one on functions that evaluate expressions

--- section-9/regular_expressions/partOne.js
+++ section-9/regular_expressions/partOne.js
@@ -4,3 +4,14 @@
 // REMEMBER forward slashes /Hello/ is an expression
 let re;
 re = /Hello/;
+
+console.log(re); // returns /Hello
+// But if you do source - it will disregard the forward slashes and return Hello
+console.log(re.source); // returns Hello
+
+
+
+
+
+// FUNCTIONS THAT ARE USED TO EVALUATE EXPRESSIONS
+
