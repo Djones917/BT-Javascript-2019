acd7f8c61f310d17d3047c8eba617f29cbda12e5
Derek <nightwing917@hotmail.com>
2019-07-20 19:06:45 -0600

    done with exec()

--- section-9/regular_expressions/partOne.js
+++ section-9/regular_expressions/partOne.js
@@ -3,7 +3,7 @@
 
 // REMEMBER forward slashes /Hello/ is an expression
 let re;
-re = /Hello/;
+re = /hello/;
 
 // console.log(re); // returns /Hello
 // But if you do source - it will disregard the forward slashes and return Hello
@@ -14,7 +14,16 @@ re = /Hello/;
 
 
 // FUNCTIONS THAT ARE USED TO EVALUATE EXPRESSIONS
-// exec() - Returns a result in array or null
-const result = re.exec('hello world');
+// exec() - Returns a result in array or null if no match NOTE: this will give an index so it could start with 0
+//const result = re.exec('hello world'); // INDEX: 0
+const result = re.exec('Derek hello world'); // INDEX: 6 because it sees my name and then finds h 
+
 console.log(result);
+console.log(result[0]);
+console.log(result.index);
+console.log(result.input);
+
+
+
+
 
