740296fb99dc2ee506e640112e774d7f4238cd0c
Derek <nightwing917@hotmail.com>
2019-07-23 18:54:43 -0600

    added /hello/g for global search

--- section-9/regular_expressions/partOne.js
+++ section-9/regular_expressions/partOne.js
@@ -3,7 +3,8 @@
 
 // REMEMBER forward slashes /Hello/ is an expression
 let re;
-re = /hello/;
+re = /hello/i; // i equal case insensitive in the test()
+re = /hello/g; // Global search
 
 // console.log(re); // returns /Hello
 // But if you do source - it will disregard the forward slashes and return Hello
@@ -14,14 +15,23 @@ re = /hello/;
 
 
 // FUNCTIONS THAT ARE USED TO EVALUATE EXPRESSIONS
+
 // exec() - Returns a result in array or null if no match NOTE: this will give an index so it could start with 0
 //const result = re.exec('hello world'); // INDEX: 0
-const result = re.exec('Derek hello world'); // INDEX: 6 because it sees my name and then finds h 
+// const result = re.exec('Derek hello world'); // INDEX: 6 because it sees my name and then finds h 
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
 
+
+
+
+
+// test() - Returns true or false
+const result = re.test('Hello'); // this will return false because of uppercase H NOTE: you can add i to the end /hello/i to make it insensitive
 console.log(result);
-console.log(result[0]);
-console.log(result.index);
-console.log(result.input);
+
 
 
 
