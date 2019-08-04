48070e6cff508ac2f55b71498aed72adac16b4ab
Derek <nightwing917@hotmail.com>
2019-08-03 19:34:40 -0600

    starting iterator

---
+++ section-10/iterators&generators/app.js
@@ -0,0 +1,8 @@
+// Iterators - are kind of like advanced loops that can be paused! Generators - are functions that can be paused and return multiple values!
+
+
+// Iterator Example
+
+function nameIterator() {
+  
+} 
