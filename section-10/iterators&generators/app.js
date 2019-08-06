7f6fb170b1db42df5e03f993546314367130bab0
Derek <nightwing917@hotmail.com>
2019-08-05 19:33:54 -0600

    starting generators

--- section-10/iterators&generators/app.js
+++ section-10/iterators&generators/app.js
@@ -1,20 +1,29 @@
-// Iterators - are kind of like advanced loops that can be paused! Generators - are functions that can be paused and return multiple values!
+// // Iterators - are kind of like advanced loops that can be paused! Generators - are functions that can be paused and return multiple values!
+
+// // Iterator Example
+
+// function nameIterator(names) {
+//   let nextIndex = 0;
+
+//   return {
+//     next: function() {
+//         return nextIndex < names.length ? { value: names[nextIndex++], done: false} : {done: true}
+//     }
+//   }
+// } 
+
+// // Create an array of names
+// const namesArr = ['Jack', 'Jill', 'John'];
+// // Initiatiate Iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value); // Jack
+// console.log(names.next().value); // Jill
+// console.log(names.next().value); // John
 
 
-// Iterator Example
-function nameIterator(names) {
-  let nextIndex = 0;
-
-  return {
-    next: function() {
-        return nextIndex < names.length ? { value: names[nextIndex++], done: false} : {done: true}
-    }
-  }
-} 
 
 
 
+// Generators
 
-
-// Create an array of names
-const nameArr = ['Jack', 'Jill', 'John'];
