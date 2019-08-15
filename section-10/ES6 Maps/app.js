a349cc395bce42b43b9de0c1a83d5f4f40fc2c0c
Derek <nightwing917@hotmail.com>
2019-08-14 19:43:45 -0600

    working on maps

--- section-10/ES6 Maps/app.js
+++ section-10/ES6 Maps/app.js
@@ -1,1 +1,11 @@
-console.log('Connected!');
+// MAPS = KEY-VALUE PAIRS - can use ANY type as a KEY or Value!
+
+const map1 = new Map();
+
+// Set Keys
+
+const key1 = 'Some String',
+      key2 = {},
+      key3 = function() {
+               console.log('Hi');
+             }
