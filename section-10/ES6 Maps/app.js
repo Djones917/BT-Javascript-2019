34b70c10f7e9b93a892ebee7609c40d4e4f170b4
Derek <nightwing917@hotmail.com>
2019-08-15 19:09:43 -0600

    adding map size

--- section-10/ES6 Maps/app.js
+++ section-10/ES6 Maps/app.js
@@ -3,9 +3,21 @@
 const map1 = new Map();
 
 // Set Keys
-
 const key1 = 'Some String',
       key2 = {},
       key3 = function() {
                console.log('Hi');
              }
+
+
+// Set map values by key
+map1.set(key1, 'Value of key1');
+map1.set(key2, 'Value of key2');
+map1.set(key3, 'Value of key3');
+
+// Get values by key
+console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+
+// We can also count the values inside a map
+console.log(map1.size); // returns Value of key1, Value of key2, Value of key3
+
