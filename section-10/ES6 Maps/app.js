91dfe5033c87767f03cdfd4cdcdd618e04f34895
Derek <nightwing917@hotmail.com>
2019-08-15 19:39:16 -0600

    added a forEach through map1

--- section-10/ES6 Maps/app.js
+++ section-10/ES6 Maps/app.js
@@ -16,8 +16,32 @@ map1.set(key2, 'Value of key2');
 map1.set(key3, 'Value of key3');
 
 // Get values by key
-console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+// console.log(map1.get(key1), map1.get(key2), map1.get(key3));
 
 // We can also count the values inside a map
-console.log(map1.size); // returns Value of key1, Value of key2, Value of key3
+// console.log(map1.size); // returns Value of key1, Value of key2, Value of key3
+
+// Iterating through maps
+// loop using for...of to get keys and values
+// for(let [key, value] of map1) {
+//   console.log(`${key} = ${value}`);
+// }
+
+// Iterate keys only
+// for (let key of map1.keys()) {
+//   console.log(key);
+// }
+
+// Iterate values only
+// for (let value of map1.values()) {
+//   console.log(value);
+// }
+
+
+
+// Loop through with forEach
+// map1.forEach(function(value, key){
+//   console.log(`${key} = ${value}`);
+// });
+
 
