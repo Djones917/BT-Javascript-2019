b264ca5b9879f992ddbe3830294ef474bd233398
Derek <nightwing917@hotmail.com>
2019-08-16 19:25:39 -0600

    added values to set1

---
+++ section-10/ES6 Sets/app.js
@@ -0,0 +1,11 @@
+// SETS - Store unique values of any type!
+
+const set1 = new Set();
+
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name: 'Derek'});
+set1.add(true);
+
+console.log(set1);
