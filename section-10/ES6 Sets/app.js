f5fa94324c21f98c42a29c33d9e8898429eb64a2
Derek <nightwing917@hotmail.com>
2019-08-16 19:41:20 -0600

    added set 2

--- section-10/ES6 Sets/app.js
+++ section-10/ES6 Sets/app.js
@@ -2,10 +2,16 @@
 
 const set1 = new Set();
 
-// Add values to set
+// Add values to set - NOTE: YOU CAN USE SET1 OR SET2 FORMATS TO ADD VALUES!
+// SET1
 set1.add(100);
 set1.add('A string');
 set1.add({name: 'Derek'});
 set1.add(true);
-
+// set1.add(100); NOTE: console.log will only print (4) it will not add the first copy because it's already there! Set values have to be unique!
 console.log(set1);
+
+
+// SET2 
+const set2 = new Set([1, true, 'string']);
+console.log(set2);
