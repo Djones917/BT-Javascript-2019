9ee8ce8134030be2b18b0b71dfc315339f4a46ea
Derek <nightwing917@hotmail.com>
2019-08-20 19:08:18 -0600

    added foreach loop

--- section-10/ES6 Sets/app.js
+++ section-10/ES6 Sets/app.js
@@ -18,16 +18,28 @@ console.log(set1);
 
 
 // Get count
-console.log(set1.size);
+// console.log(set1.size);
 
 // Check for values
-console.log(set1.has(100)); // return true
-console.log(set1.has(50 + 50)); // return true
-console.log(set1.has({name: 'John'})); // return false because it's a reference object!
+// console.log(set1.has(100)); // return true
+// console.log(set1.has(50 + 50)); // return true
+// console.log(set1.has({name: 'John'})); // return false because it's a reference object!
 // Example from data types
 //console.log({ name: 'John' } === { name: 'John' }); // Return false 
 
 // Delete from the set
-set1.delete(100);
+// set1.delete(100);
+// console.log(set1); 
+
+// Iterating through sets
+// for...0f
+for(let item of set1) {
+   console.log(item);
+}
+
+// forEach loop
+// set1.forEach((value) => 
+//   console.log(value);
+// );
 
 
