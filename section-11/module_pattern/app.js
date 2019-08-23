43f571b5684f9bcd1a0e1a96e23291f411bc827e
Derek <nightwing917@hotmail.com>
2019-08-22 19:49:40 -0600

    added uictrl

--- section-11/module_pattern/app.js
+++ section-11/module_pattern/app.js
@@ -1,10 +1,27 @@
 // Module and Revealing Module Pattern
-// Basic Structure
+// Basic Structure - IIFE Function
 
-(function() {
-  // Declare private vars and functions
+// (function() {
+//   // Declare private vars and functions
+
+//   return {
+//     // Declare public vars and functions
+//   }
+// })();
+
+
+// STANDARD MODULE PATTERN - CAN'T ACCESS WHAT'S INSIDE!
+const UICtrl = (function() {
+  let text = 'Hello World!';
+
+  const changeText = function() {
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  }
 
   return {
-    // Declare public vars and functions
+    callChangeText: function() {
+      changeText();
+    }
   }
 })();
