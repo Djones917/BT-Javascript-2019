e869f74b1dfb0c38e8933d11d28bc4952588a015
Derek <nightwing917@hotmail.com>
2019-08-26 20:18:16 -0600

    added instanceA

--- section-11/module_pattern/app.js
+++ section-11/module_pattern/app.js
@@ -66,8 +66,22 @@ const Singleton = (function() {
 
   function createInstance() {
     const object = new Object('Object instance...');
+    return object;
+  }
+
+  return {
+    getInstance: function() {
+      if(!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
   }
 })();
+
+const instanceA = Singleton.getInstance();
+
+console.log(instanceA);
 
 
 
