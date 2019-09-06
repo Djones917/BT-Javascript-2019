5a90b393785bcd5b4e37997d382c61f1d5b8ce10
Derek <nightwing917@hotmail.com>
2019-09-05 19:54:43 -0600

    starting state pattern function

--- section-11/state_pattern/app.js
+++ section-11/state_pattern/app.js
@@ -1,1 +1,12 @@
-console.log('Connected!');
+const PageState = function() {
+    let currentState = new homeState(this);
+
+    this.init = function() {
+        this.change(new homeState);
+    }
+
+    this.change = function() {
+        currentState = state;
+    }
+}
+
