6caa29d7a3ec11ac16162b20f9f7cd566131f5db
Derek <nightwing917@hotmail.com>
2019-09-07 19:38:06 -0600

    added fluid jumbotron to app js

--- section-11/state_pattern/app.js
+++ section-11/state_pattern/app.js
@@ -13,7 +13,15 @@ const PageState = function() {
 
 
 // Home State
-
 const homeState = function() {
-    
+    document.querySelector('#heading').textContent = null;
+    document.querySelector('#content').innerHTML = `
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
+      </div>
+    `;
 }
+
